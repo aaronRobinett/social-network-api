732b85b2108fed7e33fa5f35f5ca5b2e0b7b2756
Aaron <aaronrobinett@gmail.com>
2021-12-07 17:58:36 -0800

    completed user model and routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser } = require('../../controllers/user-controller');
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 module.exports = router;
